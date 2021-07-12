e5e7928634323fe007440fd7654f6cadc4d870b7
Elton M <eltonmarku@gmail.com>
2021-07-12 09:07:05 +0200

    update paths

--- render.ts
+++ render.ts
@@ -1,71 +1,75 @@
-import * as stdPath from 'https://deno.land/std@0.97.0/path/mod.ts';
-import * as eta from 'https://deno.land/x/eta@v1.6.0/mod.ts';
-import generate from 'https://x.nest.land/denoname@0.8.2/mod.ts';
+import * as stdPath from "https://deno.land/std@0.97.0/path/mod.ts";
+import * as eta from "https://deno.land/x/eta@v1.6.0/mod.ts";
+import generate from "https://x.nest.land/denoname@0.8.2/mod.ts";
 const { dirname } = generate(import.meta);
 
 eta.configure({
-  views: stdPath.join('views'),
+  views: stdPath.join("views"),
 });
 
-export const render = (jsonResume: string): string | Promise<string> | void => {
+export const render = (
+  jsonResume: string,
+  type: string,
+): string | Promise<string> | void => {
   var css = Deno.readTextFileSync(`/${dirname}/style.css`);
   var layout = Deno.readTextFileSync(`/${dirname}/layout.eta`);
 
   eta.templates.define(
-    'award',
+    "award",
     eta.loadFile(`/${dirname}/views/awards.eta`, {} as any, true),
   );
   eta.templates.define(
-    'basic',
+    "basic",
     eta.loadFile(`/${dirname}/views/basics.eta`, {} as any, true),
   );
   eta.templates.define(
-    'education',
+    "education",
     eta.loadFile(`/${dirname}/views/education.eta`, {} as any, true),
   );
   eta.templates.define(
-    'interest',
+    "interest",
     eta.loadFile(`/${dirname}/views/interests.eta`, {} as any, true),
   );
   eta.templates.define(
-    'language',
+    "language",
     eta.loadFile(`/${dirname}/views/languages.eta`, {} as any, true),
   );
   eta.templates.define(
-    'publication',
+    "publication",
     eta.loadFile(`/${dirname}/views/publications.eta`, {} as any, true),
   );
   eta.templates.define(
-    'reference',
+    "reference",
     eta.loadFile(`/${dirname}/views/references.eta`, {} as any, true),
   );
   eta.templates.define(
-    'skills',
+    "skills",
     eta.loadFile(`/${dirname}/views/skills.eta`, {} as any, true),
   );
   eta.templates.define(
-    'volunteer',
+    "volunteer",
     eta.loadFile(`/${dirname}/views/volunteer.eta`, {} as any, true),
   );
   eta.templates.define(
-    'work',
+    "work",
     eta.loadFile(`/${dirname}/views/work.eta`, {} as any, true),
   );
 
   return eta.render(layout, {
     css: css,
     resume: JSON.parse(jsonResume),
+    type: type,
     templates: [
-      'basic',
-      'award',
-      'education',
-      'interest',
-      'language',
-      'publication',
-      'reference',
-      'skills',
-      'volunteer',
-      'work',
+      "basic",
+      "award",
+      "education",
+      "interest",
+      "language",
+      "publication",
+      "reference",
+      "skills",
+      "volunteer",
+      "work",
     ],
   });
 };
--- test.ts
+++
@@ -1,6 +0,0 @@
-import { render } from "./mod.ts";
-import htmlPrettify from 'https://cdn.skypack.dev/html-prettify';
-
-const result = await render(Deno.readTextFileSync('./resume.json'));
-
-console.log(htmlPrettify(result));
