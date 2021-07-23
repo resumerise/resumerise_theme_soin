0b21e9ef250e2f3519376dab1bde8b9fea6eba63
Elton M <eltonmarku@gmail.com>
2021-07-23 16:32:57 +0200

    add export functions to mod.ts

--- run.ts
+++ run.ts
@@ -1,8 +1,8 @@
-import {render} from "./mod.ts";
+import { render } from "./mod.ts";
 import {
-    compilePNG,
-    getDefaultResume,
-    ThemeAction,
-    validateMeta,
-} from "../resumerise-theme-library/mod.ts";
+  compilePNG,
+  getDefaultResume,
+  ThemeAction,
+  validateMeta,
+} from "https://deno.land/x/resumerise_library@0.0.2/mod.ts";
 console.log(await render(getDefaultResume(), "PDF"));
