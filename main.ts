d8d3fe58fcfa459cba9c608afd7a070c7aa6dae4
Elton M <eltonmarku@gmail.com>
2021-07-27 21:59:04 +0200

    update library version

--- main.ts
+++ main.ts
@@ -1,8 +1,6 @@
 import * as stdPath from "https://deno.land/std@0.97.0/path/mod.ts";
 import * as eta from "https://deno.land/x/eta@v1.6.0/mod.ts";
-import generate from "https://x.nest.land/denoname@0.8.2/mod.ts";
-import { ResumeriseMeta } from "https://deno.land/x/resumerise_library@0.0.2/mod.ts";
-const { dirname } = generate(import.meta);
+import { ResumeriseMeta } from "https://deno.land/x/resumerise_library@0.0.3/mod.ts";
 
 eta.configure({
   views: stdPath.join("views"),
@@ -12,48 +10,48 @@ export const render = async (
   jsonResume: string,
   type: string,
 ): Promise<string> => {
-  const css = Deno.readTextFileSync(`/${dirname}/style.css`);
-  const layout = Deno.readTextFileSync(`/${dirname}/layout.eta`);
+  const css = Deno.readTextFileSync(`./style.css`);
+  const layout = Deno.readTextFileSync(`./layout.eta`);
 
   eta.templates.define(
     "award",
-    eta.loadFile(`/${dirname}/views/awards.eta`, {} as any, true),
+    eta.loadFile(`./views/awards.eta`, {} as any, true),
   );
   eta.templates.define(
     "basic",
-    eta.loadFile(`/${dirname}/views/basics.eta`, {} as any, true),
+    eta.loadFile(`./views/basics.eta`, {} as any, true),
   );
   eta.templates.define(
     "education",
-    eta.loadFile(`/${dirname}/views/education.eta`, {} as any, true),
+    eta.loadFile(`./views/education.eta`, {} as any, true),
   );
   eta.templates.define(
     "interest",
-    eta.loadFile(`/${dirname}/views/interests.eta`, {} as any, true),
+    eta.loadFile(`./views/interests.eta`, {} as any, true),
   );
   eta.templates.define(
     "language",
-    eta.loadFile(`/${dirname}/views/languages.eta`, {} as any, true),
+    eta.loadFile(`./views/languages.eta`, {} as any, true),
   );
   eta.templates.define(
     "publication",
-    eta.loadFile(`/${dirname}/views/publications.eta`, {} as any, true),
+    eta.loadFile(`./views/publications.eta`, {} as any, true),
   );
   eta.templates.define(
     "reference",
-    eta.loadFile(`/${dirname}/views/references.eta`, {} as any, true),
+    eta.loadFile(`./views/references.eta`, {} as any, true),
   );
   eta.templates.define(
     "skills",
-    eta.loadFile(`/${dirname}/views/skills.eta`, {} as any, true),
+    eta.loadFile(`./views/skills.eta`, {} as any, true),
   );
   eta.templates.define(
     "volunteer",
-    eta.loadFile(`/${dirname}/views/volunteer.eta`, {} as any, true),
+    eta.loadFile(`./views/volunteer.eta`, {} as any, true),
   );
   eta.templates.define(
     "work",
-    eta.loadFile(`/${dirname}/views/work.eta`, {} as any, true),
+    eta.loadFile(`./views/work.eta`, {} as any, true),
   );
 
   const result = await eta.render(layout, {
@@ -79,7 +77,7 @@ export const render = async (
 export const getMeta = (): ResumeriseMeta => {
   return JSON.parse(
     Deno.readTextFileSync(
-      "/Users/eltonmarku/resumerise/resumerise-theme-retro/meta.json",
+      "./meta.json",
     ),
   ) as ResumeriseMeta;
 };
--- run.ts
+++ run.ts
@@ -1,8 +1,5 @@
 import { render } from "./mod.ts";
 import {
-  compilePNG,
   getDefaultResume,
-  ThemeAction,
-  validateMeta,
-} from "https://deno.land/x/resumerise_library@0.0.2/mod.ts";
+} from "https://deno.land/x/resumerise_library@0.0.3/mod.ts";
 console.log(await render(getDefaultResume(), "PDF"));
