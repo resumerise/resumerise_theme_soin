fab382adc6337fb314a541eb79abb3a254fb9403
Elton M <eltonmarku@gmail.com>
2021-08-18 16:52:52 +0200

    refactor paths

--- README.md
+++ README.md
@@ -1,27 +1,33 @@
 # Boilerplate theme
 
-This is the boilerplate theme for [JSON Resume](http://jsonresume.org/).
+This is the boilerplate theme for [resumerise](http://resumerise.io).
 
 ## Getting started
 
 To get started with theme development, this is what you'll need:
 
 - [deno](https://deno.land/#installation)
-- [resumerise_theme_retro](https://deno.land/x/resumerise_theme_retro@0.0.5)
+- [resumerise_cli](https://deno.land/x/resumerise_cli)
+- [resumerise_theme_retro](https://deno.land/x/resumerise_theme_retro)
 
 ### Serve theme
 
-While inside the theme folder, simply run:
+Install the command-line tool:
 
 ```
-resume serve
+deno install --allow-net --allow-read --unstable -n resumerise-cli https://deno.land/x/resumerise_cli/index.ts
 ```
 
-You should now see this message:
+Start the preview server with
 
 ```
-Preview: http://localhost:4000
-Press ctrl-c to stop
+resumerise-cli serve [absolute path to your resumerise theme]/mod.ts
+```
+
+After the preview server started you should see the console output.
+
+```
+server has started on http://localhost:3000 🚀
 ```
 
 Congratulations, you've made it!
@@ -33,77 +39,23 @@ Congratulations, you've made it!
 ### Overview
 
 Now that you have your boilerplate theme installed, go through a quick overview
-of each of the files needed for your JSONResume theme:
+of each of the files needed for your resumerise theme:
 
-- `package.json`: Your package.json is required by all npm packages. Everytime
-  you want to release a new update of your theme, you'll need to update it's
-  version number.
-- `index.ts`: This is the file that will return the needed HTML to the theme
-  server. You can use it to process some things with your theme first, but we'll
-  talk about that a bit later.
-- `resume.hbs`: This is your actual template. This file is sent to the
-  `index.ts` for it to sent to the theme server.
-- `style.css`: This is where all the CSS of your project goes. Since the
-  `index.ts` only returns HTML, the contents of this file are put between
-  `<style>` tags in your `resume.hbs` file.
-
-In order to get values from an actual JSONResume, you'll need to use a
-templating system, such as [Mustache](http://mustache.github.io/) or
-[Handlebars](http://handlebarsjs.com/). The default boilerplate theme uses
-Handlebars.
-
-### index.ts
-
-The `index.ts` is where all the compiliing of your theme, and neccessary edits
-will go.
-
-At the top, you can already see the Node.js `require` function being used with
-the dependencies. You can obviously add own dependencies, if you are planning on
-using a different templating system, you can remove it.
-
-The most important part of `index.ts` is the `render` function. This is where
-all the compilation goes. This render function is expected to take a resume
-object (from a `resume.json`), and should return HTML. In this case, it is
-returning a compiled Handlebars document. If you removed the Handlebars
-dependency, you'll want to remove it and replace it with your own templating
-system compilation.
-
-Above the `return` line are css and template variables. Using the Node.js `fs`
-module, it reads first the `style.css` and the `resume.hbs`.
-
-### resume.hbs
-
-The `resume.hbs` file is where the actual template is. It contains all of the
-markup needed for your resume. By default, this is a Handlebars document, but it
-can obviously be changed into a different templating document.
+- `meta.json`: Put your theme specific information here. Everytime you want to
+  release a new update of your theme, you'll need to update it's version number.
+- `mod.ts`: [tbd]
 
 ### style.css
 
 Last but not least, the `style.css` defines your styles. Technically, this is
 completely optional, as you could just write all of your styles in the `<style>`
-tags of your `resume.hbs`. As the `index.ts`, the contents of the `style.css`
-are put into the `<style>` tags of your compiled theme later, yet again, this is
-something can change.
+tags of your `layout.eta`. The contents of the `style.css` are put into the
+`<style>` tags of your compiled theme later, yet again, this is something can
+change.
 
 ## Deployment
 
-If you are familar with NPM, you should be done with this in no time.
-
-If you already have an NPM account, you can run `npm login` and enter your
-username and password. If not, you can run `npm adduser` and fill in the proper
-fields.
-
-If you changed the dependencies or added new ones, you'll want to run
-`npm install` again, and just to make sure, run `npm update` to get the latest
-version of each dependency.
-
-When you are done with that, you may go into your package.json, and edit the
-version number. When all of the above is finished, you may run `npm publish` to
-release your theme to the public. Now everyone can use your theme with their
-resume.
-
-When updating your theme, you'll need to change the version number and run
-`npm publish` again.
+[tbd]
 
 ## License
 
--- main.ts
+++ main.ts
@@ -23,98 +23,150 @@ export const render = async (
   resume: Resume,
   type: string,
 ): Promise<string> => {
-  const css = Deno.readTextFileSync(`${__dirname}/style.css`);
-  const layout = Deno.readTextFileSync(`${__dirname}/layout.eta`);
+  const css = Deno.readTextFileSync(`${__dirname}/css/style.css`);
+  const layout = Deno.readTextFileSync(`${__dirname}/templates/layout.eta`);
 
   const awardTemplateName = "awards";
   eta.templates.define(
     awardTemplateName,
-    eta.loadFile(`${__dirname}/views/awards.eta`, {} as any, true),
+    eta.loadFile(
+      `${__dirname}/templates/categories/awards.eta`,
+      {} as any,
+      true,
+    ),
   );
 
   const basicTemplateName = "basic";
   eta.templates.define(
     basicTemplateName,
-    eta.loadFile(`${__dirname}/views/basics.eta`, {} as any, true),
+    eta.loadFile(
+      `${__dirname}/templates/categories/basics.eta`,
+      {} as any,
+      true,
+    ),
   );
 
   const profileTemplateName = "profile";
   eta.templates.define(
     profileTemplateName,
-    eta.loadFile(`${__dirname}/views/profiles.eta`, {} as any, true),
+    eta.loadFile(
+      `${__dirname}/templates/categories/profiles.eta`,
+      {} as any,
+      true,
+    ),
   );
 
   const educationTemplateName = "education";
   eta.templates.define(
     educationTemplateName,
-    eta.loadFile(`${__dirname}/views/education.eta`, {} as any, true),
+    eta.loadFile(
+      `${__dirname}/templates/categories/education.eta`,
+      {} as any,
+      true,
+    ),
   );
 
   const interestTemplateName = "interest";
   eta.templates.define(
     interestTemplateName,
-    eta.loadFile(`${__dirname}/views/interests.eta`, {} as any, true),
+    eta.loadFile(
+      `${__dirname}/templates/categories/interests.eta`,
+      {} as any,
+      true,
+    ),
   );
 
   const languageTemplateName = "language";
   eta.templates.define(
     languageTemplateName,
-    eta.loadFile(`${__dirname}/views/languages.eta`, {} as any, true),
+    eta.loadFile(
+      `${__dirname}/templates/categories/languages.eta`,
+      {} as any,
+      true,
+    ),
   );
 
   const publicationTemplateName = "publication";
   eta.templates.define(
     publicationTemplateName,
-    eta.loadFile(`${__dirname}/views/publications.eta`, {} as any, true),
+    eta.loadFile(
+      `${__dirname}/templates/categories/publications.eta`,
+      {} as any,
+      true,
+    ),
   );
 
   const referenceTemplateName = "reference";
   eta.templates.define(
     referenceTemplateName,
-    eta.loadFile(`${__dirname}/views/references.eta`, {} as any, true),
+    eta.loadFile(
+      `${__dirname}/templates/categories/references.eta`,
+      {} as any,
+      true,
+    ),
   );
 
   const skillsTemplateName = "skills";
   eta.templates.define(
     skillsTemplateName,
-    eta.loadFile(`${__dirname}/views/skills.eta`, {} as any, true),
+    eta.loadFile(
+      `${__dirname}/templates/categories/skills.eta`,
+      {} as any,
+      true,
+    ),
   );
 
   const volunteerTemplateName = "volunteer";
   eta.templates.define(
     volunteerTemplateName,
-    eta.loadFile(`${__dirname}/views/volunteer.eta`, {} as any, true),
+    eta.loadFile(
+      `${__dirname}/templates/categories/volunteer.eta`,
+      {} as any,
+      true,
+    ),
   );
 
   const workTemplateName = "work";
   eta.templates.define(
     workTemplateName,
-    eta.loadFile(`${__dirname}/views/work.eta`, {} as any, true),
+    eta.loadFile(`${__dirname}/templates/categories/work.eta`, {} as any, true),
   );
 
   const projectTemplateName = "project";
   eta.templates.define(
     projectTemplateName,
-    eta.loadFile(`${__dirname}/views/projects.eta`, {} as any, true),
+    eta.loadFile(
+      `${__dirname}/templates/categories/projects.eta`,
+      {} as any,
+      true,
+    ),
   );
 
   const certificateTemplateName = "certification";
   eta.templates.define(
     certificateTemplateName,
-    eta.loadFile(`${__dirname}/views/certifications.eta`, {} as any, true),
+    eta.loadFile(
+      `${__dirname}/templates/categories/certifications.eta`,
+      {} as any,
+      true,
+    ),
   );
 
   const dateRangeTemplateName = "date-range";
   eta.templates.define(
     dateRangeTemplateName,
-    eta.loadFile(`${__dirname}/views-other/date-range.eta`, {} as any, true),
+    eta.loadFile(
+      `${__dirname}/templates/widgets/date-range.eta`,
+      {} as any,
+      true,
+    ),
   );
 
   const keyValueTemplateName = "key-value-item";
   eta.templates.define(
     keyValueTemplateName,
     eta.loadFile(
-      `${__dirname}/views-other/key-value-item.eta`,
+      `${__dirname}/templates/widgets/key-value-item.eta`,
       {} as any,
       true,
     ),
@@ -124,7 +176,7 @@ export const render = async (
   eta.templates.define(
     listTemplateName,
     eta.loadFile(
-      `${__dirname}/views-other/list.eta`,
+      `${__dirname}/templates/widgets/list.eta`,
       {} as any,
       true,
     ),
@@ -133,13 +185,21 @@ export const render = async (
   const locationTemplateName = "location";
   eta.templates.define(
     locationTemplateName,
-    eta.loadFile(`${__dirname}/views/location.eta`, {} as any, true),
+    eta.loadFile(
+      `${__dirname}/templates/categories/location.eta`,
+      {} as any,
+      true,
+    ),
   );
 
   const profilePictureTemplateName = "profile-picture";
   eta.templates.define(
     profilePictureTemplateName,
-    eta.loadFile(`${__dirname}/views/profile-picture.eta`, {} as any, true),
+    eta.loadFile(
+      `${__dirname}/templates/categories/profile-picture.eta`,
+      {} as any,
+      true,
+    ),
   );
 
   const map = new Map<string, string>();
