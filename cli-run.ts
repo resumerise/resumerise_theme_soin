2c8116f819dae3b3973d1a511381792512ae6619
Elton M <eltonmarku@gmail.com>
2021-10-05 13:11:28 +0200

    remove obsolete files

--- cli-run.ts
+++
@@ -1,9 +0,0 @@
-import { Resume } from "../resumerise_library/codegen/model/resume.ts";
-import { render } from "./main.ts";
-
-// resume
-const resume = JSON.parse(Deno.args[0]) as Resume;
-// type
-const type = Deno.args[1];
-
-console.log(await render(resume, type));
